f4721ded27a9730a038a0b66d6460817c9e8b44d
Jeff Schuil <jeff@jetbuilt.com>
2021-01-14 14:05:03 -0800

    Use JWT to decode user info from the access token
    When using the Exchange Online scopes, together with the regular Graph API scopes, this seems to be the method that works for extacting user info from an access token.
    
    Similar to: https://github.com/marknadig/omniauth-azure-oauth2
    
    That gem didn't use Microsoft's v2.0 endpoints, however.
    
    This hybrid appears to work for my use case.
    
    e.g.
    Scope: 'offline_access https://outlook.office365.com/EAS.AccessAsUser.All https://outlook.office365.com/EWS.AccessAsUser.All openid profile User.Read'

--- lib/omniauth/strategies/microsoft_office365.rb
+++ lib/omniauth/strategies/microsoft_office365.rb
@@ -1,4 +1,5 @@
 require "omniauth/strategies/oauth2"
+require "jwt"
 
 module OmniAuth
   module Strategies
@@ -15,19 +16,19 @@ module OmniAuth
 
       option :authorize_options, %w[scope domain_hint]
 
-      uid { raw_info["id"] }
+      uid {
+        raw_info['sub']
+      }
 
       info do
         {
-          email:           raw_info["mail"] || raw_info["userPrincipalName"],
-          display_name:    raw_info["displayName"],
-          first_name:      raw_info["givenName"],
-          last_name:       raw_info["surname"],
-          job_title:       raw_info["jobTitle"],
-          business_phones: raw_info["businessPhones"],
-          mobile_phone:    raw_info["mobilePhone"],
-          office_phone:    raw_info["officePhone"],
-          image:           avatar_file,
+          name: raw_info['name'],
+          nickname: raw_info['unique_name'],
+          first_name: raw_info['given_name'],
+          last_name: raw_info['family_name'],
+          email: raw_info['email'] || raw_info['upn'],
+          oid: raw_info['oid'],
+          tid: raw_info['tid']
         }
       end
 
@@ -37,8 +38,15 @@ module OmniAuth
         }
       end
 
+      # changed default gem
+      # use JWT to parse the access token's info
+      # was the only method that seemed to work when
+      # requesting Exchange Active Sync endpoints
+      # similar to https://github.com/marknadig/omniauth-azure-oauth2
+      # (didn't use that gem because it doesn't use v2.0 endpoints)
       def raw_info
-        @raw_info ||= access_token.get("https://graph.microsoft.com/v1.0/me").parsed
+        # it's all here in JWT http://msdn.microsoft.com/en-us/library/azure/dn195587.aspx
+        @raw_info ||= ::JWT.decode(access_token.token, nil, false).first
       end
 
       def authorize_params
@@ -58,27 +66,6 @@ module OmniAuth
       def callback_url
         options[:redirect_uri] || (full_host + script_name + callback_path)
       end
-
-      def avatar_file
-        photo = access_token.get("https://graph.microsoft.com/v1.0/me/photo/$value")
-        ext   = photo.content_type.sub("image/", "") # "image/jpeg" => "jpeg"
-
-        Tempfile.new(["avatar", ".#{ext}"]).tap do |file|
-          file.binmode
-          file.write(photo.body)
-          file.rewind
-        end
-
-      rescue ::OAuth2::Error => e
-        if e.response.status == 404 # User has no avatar...
-          return nil
-        elsif e.code['code'] == 'GetUserPhoto' && e.code['message'].match('not supported')
-          nil
-        else
-          raise
-        end
-      end
-
     end
   end
 end
--- omniauth-microsoft-office365.gemspec
+++ omniauth-microsoft-office365.gemspec
@@ -22,6 +22,7 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency "omniauth"
   spec.add_runtime_dependency "omniauth-oauth2"
+  spec.add_runtime_dependency 'jwt', ['>= 1.0', '< 3.0']
 
   spec.add_development_dependency "bundler", ">= 1.6"
   spec.add_development_dependency "rake", ">= 11.1.2"
